babd6ab2be052b84b6669bed7d6aa8a04d335e4d
Benbox <benboxuwu@gmail.com>
2023-12-06 16:20:14 -0600

    index

--- src/views/HOME.js
+++ src/views/HOME.js
@@ -1,48 +1,14 @@
 import data from "../data/dataset.js";
-import { renderItems } from "../componets/view.js";
+import { renderItems } from "../componets/tarjetas.js";
 import { footer } from "../componets/footer.js";
+import { header } from "../componets/header.js";
+import { filtros } from "../componets/filtros.js";
+
 
 export const Home = () => {
   const homeView = document.createElement("section");
-  const filtrosHtml = `
-    <div class="contenedor-flex">
-
-    <label class="label-select" for="filtrar-por-género">Filtrar por género</label>
-    <select  data-testid="select-filter"  name="género"  id="filtrar-por-género">
-      <option value="nada"></option>
-      <option value="Acción">Acción</option>
-      <option value="Disparos">Disparos</option>
-      <option value="Rol">Rol</option>
-      <option value="Estrategia">Estrategia</option>
-      <option value="Aventuras">Aventuras</option>
-      <option value="Puzzle">Puzzle</option>
-      <option value="Lucha">Lucha</option>
-    </select>
-
-
-    <label class="label-select" for="filtrar-por-POV">Filtrar por POV</label>
-    <select data-testid="select-filter" name="POV"  id="filtrar-por-POV">
-      <option value="nada"></option>
-      <option value="Tercera persona">Tercera persona</option>
-      <option value="Primera persona">Primera persona</option>
-      <option value="Vista isométrica">Vista isométrica</option>
-      <option value="Vista superior">Vista superior</option>
-      <option value="Vista lateral">Vista lateral</option>
-    </select>
-    <label class="label-select" for="ordenar-por">Ordenar por</label>
-    <select data-testid="select-filter" name="alfabet" id="ordenar-por">
-      <option value="nada"></option>
-      <option value="asc">A - Z</option>
-      <option value="desc">Z - A</option>
-    </select>
-
-    <button data-testid="restablecerFiltros" name="limpiarFiltros" id="restablecer-Filtros">Restablecer Filtros</button>
+  homeView.innerHTML = header() + filtros() +  footer();
   
-    <p id="estadisticas"></p>
-  </div>`;
-  homeView += footer();
-  const conteinConteiner = document.createElement("div");
-  conteinConteiner = filtrosHtml;
   homeView.appendChild(renderItems(data));
   return homeView;
 };
