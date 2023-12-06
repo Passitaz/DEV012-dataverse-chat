1e8da869ad5581ab91b41656a70ff297da613ae4
Passitaz <zpasillas.ma@gmail.com>
2023-12-06 16:41:40 -0600

    Posición footer

--- src/views/HOME.js
+++ src/views/HOME.js
@@ -7,8 +7,14 @@ import { filtros } from "../componets/filtros.js";
 
 export const Home = () => {
   const homeView = document.createElement("section");
-  homeView.innerHTML = header() + filtros() +  footer();
+  homeView.innerHTML = header() + filtros();
   
   homeView.appendChild(renderItems(data));
+
+  const homeFooter = document.createElement("section");
+  homeFooter.innerHTML = footer();
+
+  homeView.appendChild(homeFooter);
+
   return homeView;
 };
