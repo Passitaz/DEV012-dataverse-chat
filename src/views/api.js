62288a955a47f92deb5583e4ec46825bce913e71
Passitaz <zpasillas.ma@gmail.com>
2023-12-19 10:19:01 -0600

    Ajuste de posición de botones de  página apiKey

--- src/views/api.js
+++ src/views/api.js
@@ -1,13 +1,15 @@
 export const api = () => {
     const apiKeyView = document.createElement("section");
     const pantallaApiKey = `
+    <button data-testid="boton-regresar" name="boton-regresar" id="botonRegresar">Regresar</button>
           <div class="contenedorApiKey" id="ingreso">
+          
             <p> Api Key Admin <br>
              Desde aqui puedes administrar la API Key a utilizar</p>
-            <button data-testid="boton-regresar" name="boton-regresar" id="botonRegresar">Regresar</button>
+            
           </div>
           <textarea id="miTextarea" name="miTextarea" rows="4" cols="50" placeholder="API KEY"></textarea>
-          <button data-testid="boton-enviar" name="boton-enviar" id="Enviar">Enviar</button>`;
+          <button data-testid="boton-enviar" name="boton-enviar" id="enviar">Enviar</button>`;
     apiKeyView.innerHTML = pantallaApiKey;
 
     return apiKeyView;
