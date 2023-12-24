8d1fc48b09aaf9f9c09f66c2d7d49e397eff2694
MrMidnight02 <itzel.garce@gmail.com>
2023-12-24 12:35:16 -0600

    posisionamiento correcto de la apikey y del boton enviar del chat individual

--- src/views/api.js
+++ src/views/api.js
@@ -1,15 +1,14 @@
 export const api = () => {
     const apiKeyView = document.createElement("section");
-    const pantallaApiKey = `
+    const pantallaApiKey  = `
     <button data-testid="boton-regresar" name="boton-regresar" id="botonRegresar">Regresar</button>
-          <div class="contenedorApiKey" id="ingreso">
-          
-            <p> Api Key Admin <br>
-             Desde aqui puedes administrar la API Key a utilizar</p>
-            
-          </div>
-          <textarea id="miTextarea" name="miTextarea" rows="4" cols="50" placeholder="API KEY"></textarea>
-          <button data-testid="boton-enviar" name="boton-enviar" id="enviar">Enviar</button>`;
+    <div class="contenedorApiKey" id="ingreso">
+      <p> Api Key Admin <br>
+       Desde aquí puedes administrar la API Key a utilizar</p>
+    </div>
+    <div>
+    <textarea id="apiTex" name="Api" rows="4" cols="50" placeholder="API KEY" class="apiText"></textarea>
+    <button data-testid="boton-enviar" name="boton-enviar" id="enviar">Enviar</button></div>`;
     apiKeyView.innerHTML = pantallaApiKey;
 
     return apiKeyView;
