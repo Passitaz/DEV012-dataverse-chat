4f7ce721362992f5e477f0917b5feddbcf408e01
MrMidnight02 <itzel.garce@gmail.com>
2024-01-04 13:58:41 -0600

    funcionamiento del boton regresar para la vista de la api

--- src/views/api.js
+++ src/views/api.js
@@ -1,17 +1,26 @@
-import { botonRegresar} from "../componets/botonPrincipal.js";
+import { botonRegresar } from "../componets/botonPrincipal.js";
 
 export const api = () => {
-    const apiKeyView = document.createElement("section");
-    const pantallaApiKey  = `
-    <button data-testid="boton-regresar" name="boton-regresar" id="botonRegresar">Regresar</button>
+
+  const divApiKey = document.createElement('div');
+  divApiKey.setAttribute("id", "pantallaApiKey");
+  divApiKey.appendChild(botonRegresar());
+
+  const apiKeyView = document.createElement("section");
+  const pantallaApiKey = `
     <div class="contenedorApiKey" id="ingreso">
       <p> Api Key Admin <br>
        Desde aquí puedes administrar la API Key a utilizar</p>
     </div>
     <div>
-    <textarea id="apiTex" name="Api" rows="4" cols="50" placeholder="API KEY" class="apiText"></textarea>
-    <button data-testid="boton-enviar" name="boton-enviar" id="enviar">Enviar</button></div>`;
-    apiKeyView.innerHTML = pantallaApiKey;
+      <textarea id="apiTex" name="Api" rows="4" cols="50" placeholder="API KEY" class="apiText"></textarea>
+      <button data-testid="boton-enviar" name="boton-enviar" id="enviar">Enviar</button>
+    </div>`;
 
-    return apiKeyView;
-}
+  apiKeyView.innerHTML = pantallaApiKey;
+
+
+  divApiKey.appendChild(apiKeyView);
+
+  return divApiKey;
+};
