c953c2bb03a447bdeded548d52363976b26ba4fe
Benbox <benboxuwu@gmail.com>
2024-01-05 11:16:27 -0600

    api cambios nodos

--- src/views/api.js
+++ src/views/api.js
@@ -1,24 +1,47 @@
 import { botonRegresar } from "../componets/botonPrincipal.js";
 
 export const api = () => {
-
-  const divApiKey = document.createElement('div');
+  const divApiKey = document.createElement("div");
   divApiKey.setAttribute("id", "pantallaApiKey");
   divApiKey.appendChild(botonRegresar());
 
   const apiKeyView = document.createElement("section");
-  const pantallaApiKey = `
-    <div class="contenedorApiKey" id="ingreso">
-      <p> Api Key Admin <br>
-       Desde aquí puedes administrar la API Key a utilizar</p>
-    </div>
-    <div>
-      <textarea id="apiTex" name="Api" rows="4" cols="50" placeholder="API KEY" class="apiText"></textarea>
-      <button data-testid="boton-enviar" name="boton-enviar" id="enviar">Enviar</button>
-    </div>`;
 
-  apiKeyView.innerHTML = pantallaApiKey;
+  const contenedorApiKeyDiv = document.createElement("div");
+  contenedorApiKeyDiv.id = "ingreso";
 
+  const pTexto= document.createTextNode("Api Key Admin \n Desde aquí puedes administrar la API Key a utilizar");
+  const pElemento = document.createElement("p");
+  pElemento.appendChild(pTexto);
+  contenedorApiKeyDiv.appendChild(pElemento);
+
+  const segundoDiv = document.createElement("div");
+
+  const contenidoTextarea = document.createElement("textarea");
+  contenidoTextarea.setAttribute("id","apiTex");
+  contenidoTextarea.setAttribute("name","Api");
+  contenidoTextarea.setAttribute("rows","4");
+  contenidoTextarea.setAttribute("cols","50");
+  contenidoTextarea.setAttribute("placeholder","API KEY");
+  contenidoTextarea.setAttribute("class","apiText");
+
+  const botonEnviar = document.createElement("button");
+  botonEnviar.setAttribute("data-testid","boton-enviar");
+  botonEnviar.setAttribute("name","boton-enviar");
+  botonEnviar.setAttribute("id","enviar");
+  botonEnviar.appendChild(document.createTextNode("Enviar"));
+  
+  segundoDiv.appendChild(contenidoTextarea);
+  segundoDiv.appendChild(botonEnviar);
+
+  apiKeyView.appendChild(contenedorApiKeyDiv);
+  apiKeyView.appendChild(segundoDiv);
+
+  document.body.appendChild(apiKeyView);
+
+  botonEnviar.addEventListener("click", () => {
+    localStorage.setItem("apiKey", contenidoTextarea.value);
+  });
 
   divApiKey.appendChild(apiKeyView);
 
