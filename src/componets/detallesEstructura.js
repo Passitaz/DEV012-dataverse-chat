68d79e0d8de3cb55d2c6f391ad7f3872a7f96b6e
MrMidnight02 <itzel.garce@gmail.com>
2024-01-05 16:19:21 -0600

    creacion del componente chatIndividual

--- src/componets/detallesEstructura.js
+++ src/componets/detallesEstructura.js
@@ -1,7 +1,6 @@
 import { renderDetails } from "../componets/detalles.js";
 
 export const characterDetail = () => {
-  
   const detallesDelPersonaje = `
     <div class=contenedorPrincipal>
     <div class="detallesJuegos2" id="detallesJuegos">
@@ -11,5 +10,5 @@ export const characterDetail = () => {
     <textarea id=chatIndividual placeholder="Escribe aquí tu mensaje"></textarea>
     <button data-testid="boton-enviar" name="boton-enviar class="enviar-chat">Enviar</button>
     </div></div></div>`;
-  return detallesDelPersonaje;};
-  
+  return detallesDelPersonaje;
+};
