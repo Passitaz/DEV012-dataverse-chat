fb2dd8b307f70723f942013a941d973606b9e5c8
Benbox <benboxuwu@gmail.com>
2024-01-08 10:33:56 -0600

    test dataFunctions

---
+++ test/data.js
@@ -0,0 +1,303 @@
+export const data = [
+  {
+    id: "street-fighter-ii",
+    name: "Street Fighter II",
+    shortDescription:
+      "Luchadores de todo el mundo se enfrentan en intensos combates uno contra uno.",
+    description:
+      "Street Fighter II es un juego de lucha desarrollado por Capcom. El juego se ha convertido en un clásico del género de juegos de lucha y ha inspirado numerosas secuelas y adaptaciones. Los jugadores eligen entre una variedad de luchadores, cada uno con sus propios movimientos y técnicas especiales, y se enfrentan en combates uno contra uno. Street Fighter II es conocido por su jugabilidad profunda y estratégica, así como por su competición multijugador en los salones recreativos y consolas. Es un título fundamental en la historia de los juegos de lucha.",
+    imageUrl: "https://i.blogs.es/f476ec/streetfighter/1366_2000.jpg",
+    facts: {
+      yearReleased: "1991",
+      gender: "Lucha",
+      playingStyles: ["2D, peleas uno contra uno"],
+      pointsOfView: "Vista lateral",
+      multiplayer: "Sí",
+    },
+  },
+  {
+    id: "doom",
+    name: "Doom",
+    shortDescription:
+      "Enfréntate a hordas de demonios en este clásico shooter en primera persona.",
+    description:
+      "Doom es un videojuego de disparos en primera persona desarrollado por id Software. El juego es conocido por su acción rápida y frenética, así como por su temática de lucha contra demonios y criaturas del infierno. Los jugadores asumen el papel de un marine espacial solitario que debe abrirse paso a través de niveles infernales llenos de enemigos. Doom ha sido influyente en el género de los juegos de disparos en primera persona y ha dado lugar a numerosas secuelas y mods creados por la comunidad de jugadores.",
+    imageUrl:
+      "https://cdn.cloudflare.steamstatic.com/steam/apps/2280/header.jpg?t=1663861909",
+    facts: {
+      yearReleased: "1993",
+      gender: "Disparos",
+      playingStyles: ["Shooter en primera persona"],
+      pointsOfView: "Primera persona",
+      multiplayer: "Sí",
+    },
+  },
+  {
+    id: "minecraft",
+    name: "Minecraft",
+    shortDescription:
+      "Crea y explora un mundo de bloques en este juego de construcción y supervivencia.",
+    description:
+      "Minecraft es un juego de construcción y supervivencia creado por Markus Persson. Los jugadores se sumergen en un mundo abierto lleno de bloques y tienen la libertad de construir, explorar y sobrevivir como prefieran. El juego presenta modos de juego creativo y de supervivencia, lo que permite a los jugadores enfrentarse a peligros como monstruos y explorar paisajes infinitos. Minecraft se ha convertido en un fenómeno global y ha inspirado una comunidad activa de creadores de contenido y modificaciones.",
+    imageUrl:
+      "https://as01.epimg.net/betech/imagenes/2017/09/20/portada/1505936498_948978_1505938201_noticia_normal.jpg",
+    facts: {
+      yearReleased: "2011",
+      gender: "Aventuras",
+      playingStyles: ["Construcción", "Supervivencia"],
+      pointsOfView: "Primera persona",
+      multiplayer: "Sí",
+    },
+  },
+  {
+    id: "fallout-3",
+    name: "Fallout 3",
+    shortDescription:
+      "Sobrevive en un mundo postapocalíptico y busca a tu padre en este juego de rol.",
+    description:
+      "Fallout 3 es un juego de rol de mundo abierto desarrollado por Bethesda Game Studios. El juego se desarrolla en un mundo devastado por la guerra nuclear, y los jugadores asumen el papel de un habitante del Refugio 101 en busca de su padre desaparecido. El juego combina elementos de exploración, combate y toma de decisiones, y los jugadores pueden interactuar con un mundo lleno de facciones, mutantes y tecnología retrofuturista. Fallout 3 es conocido por su narrativa rica y su mundo postapocalíptico detallado.",
+    imageUrl:
+      "https://store-images.s-microsoft.com/image/apps.43444.13574127358458005.65070c14-0cf2-44ab-b4fc-c66107fdb092.5acef951-2322-424c-93ad-6bfc679a27ef?q=90&w=480&h=270",
+    facts: {
+      yearReleased: "2008",
+      gender: "Rol",
+      playingStyles: ["Mundo abierto", "Aventuras"],
+      pointsOfView: "Primera persona",
+      multiplayer: "No",
+    },
+  },
+  {
+    id: "the-elder-scrolls-v-skyrim",
+    name: "The Elder Scrolls V: Skyrim",
+    shortDescription:
+      "Explora un mundo de fantasía épico y embárcate en una búsqueda heroica en Skyrim.",
+    description:
+      "The Elder Scrolls V: Skyrim es un juego de rol de mundo abierto desarrollado por Bethesda Game Studios. El juego se desarrolla en la provincia de Skyrim y presenta un vasto mundo lleno de aventuras, criaturas y misterios. Los jugadores asumen el papel de un Dovahkiin, un héroe destinado a enfrentarse al dragón Alduin. El juego ofrece una jugabilidad no lineal, lo que permite a los jugadores tomar decisiones que afectan la historia y explorar un mundo lleno de misiones secundarias y secretos. Skyrim se ha convertido en un título emblemático de los juegos de rol occidentales.",
+    imageUrl: "https://gepig.com/game_cover_460w/3557.jpg",
+    facts: {
+      yearReleased: "2011",
+      gender: "Rol",
+      playingStyles: ["Mundo abierto", "Aventuras"],
+      pointsOfView: "Primera persona",
+      multiplayer: "No",
+    },
+  },
+  {
+    id: "portal",
+    name: "Portal",
+    shortDescription:
+      "Resuelve rompecabezas en este juego de plataformas en primera persona con un portal de teletransporte.",
+    description:
+      "Portal es un juego de plataformas en primera persona desarrollado por Valve. El juego presenta una jugabilidad única basada en la resolución de rompecabezas utilizando un dispositivo de teletransporte llamado Portal Gun. Los jugadores asumen el papel de Chell, una mujer que se despierta en las instalaciones de Aperture Science y debe resolver una serie de desafíos diseñados por la inteligencia artificial GLaDOS. Portal es conocido por su humor negro y su ingenioso diseño de rompecabezas, lo que lo convierte en un título icónico en el mundo de los juegos de lógica y resolución de acertijos.",
+    imageUrl:
+      "https://cdn.cloudflare.steamstatic.com/steam/apps/400/capsule_616x353.jpg?t=1696346380",
+    facts: {
+      yearReleased: 2007,
+      gender: "Puzzle",
+      playingStyles: ["Puzzle", "Shooter en primera persona"],
+      pointsOfView: "Primera persona",
+      multiplayer: "No",
+    },
+  },
+  {
+    id: "bioshock-infinite",
+    name: "BioShock Infinite",
+    shortDescription:
+      "Explora la ciudad flotante de Columbia en este juego de disparos en primera persona con elementos de ciencia ficción.",
+    description:
+      "BioShock Infinite es un juego de disparos en primera persona desarrollado por Irrational Games. El juego se desarrolla en la ciudad flotante de Columbia y sigue la historia de Booker DeWitt, un investigador privado que debe rescatar a Elizabeth, una joven con habilidades especiales, de las garras del líder de Columbia, Comstock. BioShock Infinite combina elementos de disparos, exploración y narrativa en un mundo vibrante y complejo. El juego aborda temas como la religión, el nacionalismo y la libertad.",
+    imageUrl:
+      "https://i.blogs.es/68073b/650_1000_bioshock-infinite-llegara-a-linux/450_1000.jpg",
+    facts: {
+      yearReleased: "2013",
+      gender: "Disparos",
+      playingStyles: ["Shooter en primera persona"],
+      pointsOfView: "Primera persona",
+      multiplayer: "No",
+    },
+  },
+  {
+    id: "the-elder-scrolls-iv-oblivion",
+    name: "The Elder Scrolls IV: Oblivion",
+    shortDescription:
+      "Embárcate en una epopeya de fantasía y aventura en el mundo de Cyrodiil.",
+    description:
+      "The Elder Scrolls IV: Oblivion es un juego de rol de mundo abierto desarrollado por Bethesda Game Studios. El juego se desarrolla en la provincia de Cyrodiil y presenta un mundo expansivo lleno de misiones, criaturas y secretos. Los jugadores asumen el papel de un prisionero liberado que se convierte en el héroe destinado a detener una amenaza demoníaca. Oblivion es conocido por su jugabilidad no lineal y la libertad que ofrece a los jugadores para explorar y tomar decisiones. Es un título emblemático de la serie Elder Scrolls.",
+    imageUrl:
+      "https://i0.wp.com/xboxera.com/wp-content/uploads/2021/03/U69uTJH.jpg?fit=1920%2C1080&ssl=1",
+    facts: {
+      yearReleased: "2006",
+      gender: "Rol",
+      playingStyles: ["Mundo abierto", "Aventuras"],
+      pointsOfView: "Primera persona",
+      multiplayer: "No",
+    },
+  },
+  {
+    id: "counter-strike-global-offensive",
+    name: "Counter-Strike: Global Offensive",
+    shortDescription:
+      "Participa en combates tácticos en equipos como terroristas o fuerzas especiales.",
+    description:
+      "Counter-Strike: Global Offensive (CS: GO) es un juego de disparos en primera persona desarrollado por Valve y Hidden Path Entertainment. El juego es una versión actualizada de la serie Counter-Strike y se centra en partidas competitivas en equipos. Los jugadores se dividen en terroristas y fuerzas especiales, y deben completar objetivos específicos o eliminar al equipo contrario. CS: GO es conocido por su juego táctico, estratégico y habilidades de puntería. Es un título popular en los deportes electrónicos (eSports).",
+    imageUrl:
+      "https://media.steampowered.com/apps/csgo/blog/images/fb_image.png?v=6",
+    facts: {
+      yearReleased: "2012",
+      gender: "Disparos",
+      playingStyles: ["Multijugador en línea", "Tácticas"],
+      pointsOfView: "Primera persona",
+      multiplayer: "Sí",
+    },
+  },
+  {
+    id: "half-life",
+    name: "Half-Life",
+    shortDescription:
+      "Juega como Gordon Freeman en esta aventura de ciencia ficción en primera persona.",
+    description:
+      "Half-Life es un juego de disparos en primera persona desarrollado por Valve. El juego presenta una narrativa de ciencia ficción en la que los jugadores asumen el papel de Gordon Freeman, un científico atrapado en un centro de investigación secreto después de un experimento fallido. Deben enfrentarse a criaturas alienígenas y resolver acertijos para sobrevivir. Half-Life es conocido por su inmersión y su enfoque en la narrativa en tiempo real. Ha dado lugar a secuelas y expansiones muy populares.",
+    imageUrl:
+      "https://cdn.hobbyconsolas.com/sites/navi.axelspringer.es/public/media/image/2020/02/half-life-1877229.jpg?tf=3840x",
+    facts: {
+      yearReleased: "1998",
+      gender: "Disparos",
+      playingStyles: ["Ciencia ficción", "Puzles"],
+      pointsOfView: "Primera persona",
+      multiplayer: "Sí",
+    },
+  },
+  {
+    id: "overwatch",
+    name: "Overwatch",
+    shortDescription:
+      "Participa en emocionantes combates en equipos con héroes únicos y habilidades especiales.",
+    description:
+      "Overwatch es un juego de disparos en primera persona desarrollado por Blizzard Entertainment. El juego se centra en partidas en equipos de seis jugadores, cada uno seleccionando entre una variedad de héroes con habilidades únicas. Los jugadores trabajan juntos para completar objetivos y derrotar al equipo contrario. Overwatch es conocido por su enfoque en la jugabilidad cooperativa y competitiva, así como por su diverso elenco de personajes. Ha sido un título popular en los deportes electrónicos.",
+    imageUrl:
+      "https://phantom-marca.unidadeditorial.es/8d87143c78ee004466437439e5df364d/resize/1200/f/webp/assets/multimedia/imagenes/2022/10/03/16648149397801.jpg",
+    facts: {
+      yearReleased: "2016",
+      gender: "Disparos",
+      playingStyles: ["Multijugador en línea", "Héroes"],
+      pointsOfView: "Primera persona",
+      multiplayer: "Sí",
+    },
+  },
+  {
+    id: "red-dead-redemption-2",
+    name: "Red Dead Redemption 2",
+    shortDescription:
+      "Embárcate en una epopeya del viejo oeste como Arthur Morgan en este juego de mundo abierto.",
+    description:
+      "Red Dead Redemption 2 es un juego de acción-aventura desarrollado por Rockstar Studios. El juego se desarrolla en el Viejo Oeste y sigue la historia de Arthur Morgan, un forajido miembro de la pandilla de Dutch van der Linde. Los jugadores pueden explorar un mundo abierto lleno de paisajes impresionantes, vida silvestre y actividades variadas. El juego combina narrativa intensa con una jugabilidad de mundo abierto, lo que permite a los jugadores decidir su propio camino y enfrentarse a las consecuencias de sus elecciones. Red Dead Redemption 2 es ampliamente elogiado por su inmersión y atención al detalle.",
+    imageUrl: "https://i.blogs.es/169695/red0/1366_2000.jpg",
+    facts: {
+      yearReleased: "2018",
+      gender: "Acción",
+      playingStyles: ["Mundo abierto", "Aventuras"],
+      pointsOfView: "Tercera persona",
+      multiplayer: "Sí",
+    },
+  },
+  {
+    id: "fortnite",
+    name: "Fortnite",
+    shortDescription:
+      "Enfréntate en emocionantes batallas en línea de Battle Royale en un mundo colorido.",
+    description:
+      "Fortnite es un juego en línea desarrollado por Epic Games. El juego se divide en dos modos principales: Salvar el mundo y Battle Royale. El modo Battle Royale ha ganado una inmensa popularidad con su mecánica de supervivencia en la que 100 jugadores se enfrentan en una isla, recolectan recursos y luchan hasta quedar un único superviviente. Fortnite es conocido por su estética colorida, trajes personalizables y eventos en vivo. Ha sido un fenómeno en la cultura pop y los eSports.",
+    imageUrl:
+      "https://static.13.cl/7/sites/default/files/esports/articulos/field-image/portadafortnite_10.jpg",
+    facts: {
+      yearReleased: "2017",
+      gender: "Acción",
+      playingStyles: ["Battle Royale", "Construcción"],
+      pointsOfView: "Tercera persona",
+      multiplayer: "Sí",
+    },
+  },
+  {
+    id: "grand-theft-auto-v",
+    name: "Grand Theft Auto V",
+    shortDescription:
+      "Explora el mundo abierto de Los Santos y participa en actividades delictivas y misiones.",
+    description:
+      "Grand Theft Auto V (GTA V) es un juego de acción-aventura desarrollado por Rockstar North. El juego se desarrolla en la ficticia ciudad de Los Santos y sus alrededores. Los jugadores asumen los roles de tres personajes principales y pueden alternar entre ellos. GTA V es conocido por su mundo abierto, su narrativa ramificada y su libertad para realizar actividades delictivas. También incluye un modo en línea llamado Grand Theft Auto Online, que permite a los jugadores explorar y competir en el mismo mundo.",
+    imageUrl:
+      "https://www.cinemascomics.com/wp-content/uploads/2014/09/gtav_ng.jpg",
+    facts: {
+      yearReleased: "2013",
+      gender: "Acción",
+      playingStyles: ["Mundo abierto", "Crimen"],
+      pointsOfView: "Tercera persona",
+      multiplayer: "Sí",
+    },
+  },
+  {
+    id: "god-of-war",
+    name: "God of War",
+    shortDescription:
+      "Acompaña a Kratos en su viaje para enfrentar a los dioses y redimir sus pecados.",
+    description:
+      "God of War es una serie de juegos de acción y aventuras desarrollada por Santa Monica Studio. El protagonista, Kratos, es un guerrero espartano que busca vengarse de los dioses del Olimpo que lo traicionaron. Acompaña a Kratos en su viaje para enfrentar a criaturas mitológicas y resolver puzles en un mundo épico. La serie es conocida por su jugabilidad intensa y su narrativa épica, y el juego más reciente, lanzado en 2018, reinventó la franquicia con un enfoque en la relación entre Kratos y su hijo, Atreus.",
+    imageUrl: "https://i.ytimg.com/vi/_20c_V2Er88/maxresdefault.jpg",
+    facts: {
+      yearReleased: "2005",
+      gender: "Acción",
+      playingStyles: ["Hack and slash", "Aventuras"],
+      pointsOfView: "Tercera persona",
+      multiplayer: "No",
+    },
+  },
+  {
+    id: "league-of-legends",
+    name: "League of Legends",
+    shortDescription:
+      "Participa en emocionantes partidas en línea con campeones y estrategias únicas.",
+    description:
+      "League of Legends (LoL) es un popular juego en línea desarrollado por Riot Games. El juego se enmarca en el género de los juegos MOBA (Arenas de batalla en línea multijugador). Los jugadores eligen campeones con habilidades únicas y se enfrentan en partidas en equipos con el objetivo de destruir el nexo del equipo contrario. League of Legends es conocido por su diversa selección de campeones, estrategias y juego en equipo. Ha sido un pionero en los deportes electrónicos y es uno de los juegos en línea más jugados del mundo.",
+    imageUrl:
+      "https://esports.eldesmarque.com/wp-content/uploads/2019/09/LoL2.jpg",
+    facts: {
+      yearReleased: "2009",
+      gender: "Estrategia",
+      playingStyles: ["Acción"],
+      pointsOfView: "Vista isométrica",
+      multiplayer: "Sí",
+    },
+  },
+  {
+    id: "dota-2",
+    name: "Dota 2",
+    shortDescription:
+      "Participa en partidas en línea estratégicas con héroes poderosos en este juego de batalla de equipos.",
+    description:
+      "Dota 2 es un juego en línea desarrollado por Valve. El juego se enmarca en el género de los juegos MOBA y es una secuela del popular mod de Warcraft III, Defense of the Ancients. Los jugadores eligen héroes con habilidades únicas y se enfrentan en partidas en equipos con el objetivo de destruir el antiguo del equipo contrario. Dota 2 es conocido por su profundidad estratégica, su alto nivel de competición y su comunidad activa. Ha sido un título importante en los deportes electrónicos (eSports).",
+    imageUrl: "https://i.blogs.es/de368e/dota-2-wallpaper/1366_2000.jpg",
+    facts: {
+      yearReleased: "2013",
+      gender: "Estrategia",
+      playingStyles: ["MOBA", "Multijugador en línea"],
+      pointsOfView: "Vista isométrica",
+      multiplayer: "Sí",
+    },
+  },
+  {
+    id: "world-of-warcraft",
+    name: "World of Warcraft",
+    shortDescription:
+      "Adéntrate en un vasto mundo de fantasía y completa misiones en línea con jugadores de todo el mundo.",
+    description:
+      "World of Warcraft (WoW) es un juego de rol en línea masivo (MMORPG) desarrollado por Blizzard Entertainment. Los jugadores eligen una facción y una raza, crean un personaje y se aventuran en el mundo de Azeroth. El juego ofrece una amplia variedad de misiones, mazmorras y raides, así como un mundo abierto para explorar. WoW es conocido por su comunidad activa y su compromiso constante con la expansión de contenidos. Ha sido un pionero en el género MMORPG.",
+    imageUrl:
+      "https://cdn.computerhoy.com/sites/navi.axelspringer.es/public/media/image/2017/04/world-warcraft.jpg",
+    facts: {
+      yearReleased: "2004",
+      gender: "Rol en línea masivo",
+      playingStyles: ["MMORPG", "Fantasía"],
+      pointsOfView: "Tercera persona",
+      multiplayer: "Sí",
+    },
+  },
+];
